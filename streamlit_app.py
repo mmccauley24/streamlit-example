871956962aa8e68f124cce910aa4fb00074795b1
Matt McCauley <mmccauley24@gmail.com>
2024-03-02 14:19:20 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -100,53 +100,13 @@ default_title2 = 'Amazing Stories'
 col1, col2 = st.columns(2)
 
 with col1:
-    title_dropdown1 = st.selectbox('Title 1:', [''] + list(unique_titles), index=np.where(unique_titles == default_title1)[0][0] + 1)
-    search_box1 = st.text_input('Search Term for Title 1:')
-    st.text(" ")  # Add empty space for better alignment
+    title_dropdown1 = st.selectbox('Title 1:', [''] + list(unique_titles), index=np.where(unique_titles == default_title1)[0][0], key='title1')
+    search_box1 = st.text_input('Search Term for Title 1:', key='search1')
 
 with col2:
-    title_dropdown2 = st.selectbox('Title 2:', [''] + list(unique_titles), index=np.where(unique_titles == default_title2)[0][0] + 1)
-    search_box2 = st.text_input('Search Term for Title 2:')
-    st.text(" ")  # Add empty space for better alignment
+    title_dropdown2 = st.selectbox('Title 2:', [''] + list(unique_titles), index=np.where(unique_titles == default_title2)[0][0], key='title2')
+    search_box2 = st.text_input('Search Term for Title 2:', key='search2')
 
 # Create button to update plot
 if st.button('Update Plot'):
     update_plot(title_dropdown1, title_dropdown2, search_box1, search_box2)
-with col1:
-    title_dropdown1 = st.selectbox('Title 1:', [''] + list(unique_titles), index=0)
-    search_box1 = st.text_input('Search Term for Title 1:')
-    st.text(" ")  # Add empty space for better alignment
-
-with col2:
-    title_dropdown2 = st.selectbox('Title 2:', [''] + list(unique_titles), index=0)
-    search_box2 = st.text_input('Search Term for Title 2:')
-    st.text(" ")  # Add empty space for better alignment
-
-# Create button to update plot
-if st.button('Update Plot'):
-    update_plot(title_dropdown1, title_dropdown2, search_box1, search_box2)
-# num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-# num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-# indices = np.linspace(0, 1, num_points)
-# theta = 2 * np.pi * num_turns * indices
-# radius = indices
-
-# x = radius * np.cos(theta)
-# y = radius * np.sin(theta)
-
-# df = pd.DataFrame({
-#     "x": x,
-#     "y": y,
-#     "idx": indices,
-#     "rand": np.random.randn(num_points),
-# })
-
-# st.altair_chart(alt.Chart(df, height=700, width=700)
-#     .mark_point(filled=True)
-#     .encode(
-#         x=alt.X("x", axis=None),
-#         y=alt.Y("y", axis=None),
-#         color=alt.Color("idx", legend=None, scale=alt.Scale()),
-#         size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-#     ))
