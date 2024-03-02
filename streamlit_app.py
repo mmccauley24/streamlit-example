9b121113d509b93088e6aa1830c3a209e1581787
Matt McCauley <mmccauley24@gmail.com>
2024-03-02 14:23:48 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,12 +96,12 @@ def update_plot(title1, title2, search_term1, search_term2):
 col1, col2 = st.columns(2)
 
 with col1:
-    title_dropdown1 = st.selectbox('Title 1:', [''] + list(unique_titles), index='Weird Tales')
+    title_dropdown1 = st.selectbox('Title 1:', [''] + list(unique_titles), index=1)
     search_box1 = st.text_input('Search Term for Title 1:')
     st.text(" ")  # Add empty space for better alignment
 
 with col2:
-    title_dropdown2 = st.selectbox('Title 2:', [''] + list(unique_titles), index='Amazing Stories')
+    title_dropdown2 = st.selectbox('Title 2:', [''] + list(unique_titles), index=2)
     search_box2 = st.text_input('Search Term for Title 2:')
     st.text(" ")  # Add empty space for better alignment
 
